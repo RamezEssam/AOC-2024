c84e8a25c568f9d54d8cad89b828dc4db88e054e
Ramez <ramez_essam_386@hotmail.com>
2024-12-13 12:17:34 +0200

    DAY6 Part 2

--- DAY6/src/day6.cpp
+++ DAY6/src/day6.cpp
@@ -8,8 +8,12 @@
 #include <stdexcept>
 #include <tuple>
 #include <unordered_set>
+#include <thread>
+#include <chrono> // For duration literals
 
 const char* INPUT_FILE_PATH = "..\\..\\DAY6\\input\\input.txt";
+
+
 
 typedef std::vector<std::vector<char>> Grid;
 
@@ -30,6 +34,15 @@ struct Position {
 };
 
 
+struct State {
+	int x, y;
+	Direction dir;
+	bool operator==(const State& other) const {
+		return x == other.x && y == other.y && dir == other.dir;
+	}
+};
+
+
 namespace std {
 	template <>
 	struct hash<Position> {
@@ -38,6 +51,19 @@ namespace std {
 			size_t h1 = std::hash<int>()(pos.x);
 			size_t h2 = std::hash<int>()(pos.y);
 			return h1 ^ (h2 << 1); // Combine the hashes
+		}
+	};
+}
+
+namespace std {
+	template <>
+	struct hash<State> {
+		size_t operator()(const State& st) const {
+			// Hash individual members and combine them
+			size_t h1 = std::hash<int>()(st.x);
+			size_t h2 = std::hash<int>()(st.y);
+			size_t h3 = std::hash<int>()(static_cast<int>(st.dir));
+			return h1 ^ (h2 << 1) ^ (h3 << 2); // Combine the hashes
 		}
 	};
 }
@@ -89,7 +115,6 @@ Position find_guard(Grid input) {
 }
 
 size_t part1(Grid input) {
-	//int result = 0;
 	Position guard = find_guard(input);
 	std::unordered_set<Position> hashSet;
 	int y_pos = guard.x;
@@ -142,12 +167,95 @@ size_t part1(Grid input) {
 	return hashSet.size();
 }
 
+
+
+int part2(Grid input) {
+
+	Position guard = find_guard(input);
+	std::unordered_set<State> hashSet;
+
+	int y_pos = guard.x;
+	int x_pos = guard.y;
+	Direction dir = guard.dir;
+	if (y_pos == -1 && x_pos == -1) {
+		return 0;
+	}
+
+	int result = 0;
+	for (int i = 0; i < input.size(); ++i) {
+		for (int j = 0; j < input[0].size(); ++j) {
+			hashSet.clear();
+			char prev_pos = input[i][j];
+			if (input[i][j] != input[guard.x][guard.y]) {
+				input[i][j] = '#';
+			}
+			y_pos = guard.x;
+			x_pos = guard.y;
+			dir = guard.dir;
+
+			while (y_pos < input.size() && x_pos < input[0].size() && y_pos >= 0 && x_pos >= 0) {
+
+				if (input[y_pos][x_pos] != '#') {
+					if (hashSet.find(State{ y_pos, x_pos, dir }) != hashSet.end()) {
+						result++;
+						break;
+					}
+					hashSet.insert(State{ y_pos, x_pos, dir });	
+				}
+				else {
+					if (dir == Direction::Up) {
+						y_pos++;
+						dir = Direction::Right;
+						hashSet.insert(State{ y_pos, x_pos, dir });
+					}
+					else if (dir == Direction::Down) {
+						y_pos--;
+						dir = Direction::Left;
+						hashSet.insert(State{ y_pos, x_pos, dir });
+					}
+					else if (dir == Direction::Left) {
+						x_pos++;
+						dir = Direction::Up;
+						hashSet.insert(State{ y_pos, x_pos, dir });
+					}
+					else if (dir == Direction::Right) {
+						x_pos--;
+						dir = Direction::Down;
+						hashSet.insert(State{ y_pos, x_pos, dir });
+					}
+				}
+
+				if (dir == Direction::Up) {
+					y_pos--;
+				}
+				else if (dir == Direction::Down) {
+					y_pos++;
+				}
+				else if (dir == Direction::Left) {
+					x_pos--;
+				}
+				else if (dir == Direction::Right) {
+					x_pos++;
+				}
+			}
+			input[i][j] = prev_pos;
+		}
+	}
+	return result;
+
+}
+
 int main() {
 	Grid input = read_input(INPUT_FILE_PATH);
 
 	size_t part1_answer = part1(input);
 
 	std::cout << "PART 1: " << part1_answer << std::endl;
+
+	size_t part2_answer = part2(input);
+
+	std::cout << "PART 2: " << part2_answer << std::endl;
+
 	
 	return 0;
 }
